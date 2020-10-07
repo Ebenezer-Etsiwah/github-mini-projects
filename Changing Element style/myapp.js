d94b40c63ff8a738cffaedf101767e61b4cba92b
Ebenezer <ETSIWAH@GMAIL.COM>
2020-10-07 14:36:58 +0000

    Resource for Element styling in Javascript

---
+++ Changing Element style/myapp.js
@@ -0,0 +1,13 @@
+//changing Element's style
+var el = document.getElementById('style')
+
+/*el.style.background ="blue";
+el.style.color ="white";
+el.style.height ="200px";
+
+el.style.CSSText ="background:blue;color:white;width:200px";
+
+el.style.CSSText = "height:100px;";*/
+
+console.log(getComputedStyle(el));
+
