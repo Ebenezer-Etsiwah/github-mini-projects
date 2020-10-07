1b29bc7c6b9344c82a5bae20c39f83998c328035
Ebenezer <ETSIWAH@GMAIL.COM>
2020-10-07 12:01:56 +0000

    Comparison opertors in Javascript

---
+++ Comparison operators/myapp.js
@@ -0,0 +1,13 @@
+//comparison operators
+var name=23;
+var name1=24
+
+// 10> 100 answer would be false
+
+//null !=undefined would be false
+
+//null!==undefined would be true
+
+//23==23 would be true
+
+// name==name1 would be false.
