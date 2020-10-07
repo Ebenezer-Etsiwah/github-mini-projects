6fc2e581fbe0fbc4d843eff84112047e46d9ac64
Ebenezer <ETSIWAH@GMAIL.COM>
2020-10-07 12:44:52 +0000

    For loops in Javascript

---
+++ For & for in loops/myapp.js
@@ -0,0 +1,16 @@
+//loops help us perform iterative processes without necessarily repeating those steps.
+var classRegister =["Lawrence","John","Jeff"];
+
+/*for(var i= 0;i<classRegister.length;i++){
+	console.log(classRegister[i])
+
+}*/
+//For decrement
+/*for(var i=classRegister.length-1;i>=0;i--){
+	console.log(classRegister[i])
+
+}*/
+for(var index in classRegister){
+console.log(classRegister[index])
+}
+
