67262c3eae773bef36c7dddcd090c7ad2d43d34c
Ebenezer <ETSIWAH@GMAIL.COM>
2020-10-07 11:24:20 +0000

    Constructors in Javascript

---
+++ Constructors/myapp.js
@@ -0,0 +1,13 @@
+//constructor
+function Apple(x,y,color,score)
+{
+this ={ x:10 };
+this.x = x;
+this.y = y;
+this.color = color;
+this.score = score;
+}
+//creat a variable in order to creat instances of the function
+var apple1=new Apple(10,20,"red",200);
+var apple2=new Apple(100,200,"green",50);
+var apple1=new Apple(20,200,"pink",10);
